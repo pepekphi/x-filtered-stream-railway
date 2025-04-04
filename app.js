2987c8b9f94194a8ab9973618a63a3de702742cf
diegodubai <dirkt82@hotmail.com>
2025-04-04 22:47:49 +0400

    Update app.js
    Sequential version again, because the last version (with concurrency) resulted in blank rows sometimes since Google Sheets webhook does not allow concurrency.

--- app.js
+++ app.js
@@ -140,14 +140,7 @@ async function startStream() {
         ? includes.users[0].username
         : "unknown";
       console.log(`New tweet detected: ${data.id} from @${usernameForLog}`);
-
-      forwardTweet(data, includes)
-        .then(() => {
-          console.log(`Tweet ${data.id} from @${usernameForLog} forwarded asynchronously.`);
-        })
-        .catch((error) => {
-          console.error(`Error forwarding tweet ${data.id}:`, error);
-        });
+      await forwardTweet(data, includes);
     }
   } catch (error) {
     if (error && error.code === 429) {
